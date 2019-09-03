bdeafe3ed6fe6a3a2426710e950534f09e0cc9f2
Emil Folino <emilfolino@gmail.com>
2019-09-03 20:05:16 +0200

    Fixed production bundle and used tree-shaking

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import slideorama from "slideorama";
 
-import hljs from 'highlight.js';
+import hljs from 'highlight.js/lib/highlight';
+import javascript from 'highlight.js/lib/languages/javascript';
+import cpp from 'highlight.js/lib/languages/cpp';
+import x86asm from 'highlight.js/lib/languages/x86asm';
+hljs.registerLanguage('x86asm', x86asm);
+hljs.registerLanguage('cpp', cpp);
+hljs.registerLanguage('javascript', javascript);
 import "highlight.js/styles/atom-one-dark.css";
 
 slideorama.init("presentation", "slide-container");

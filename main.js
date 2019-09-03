32fd59b586c791c5f6c111a661faec3c64987499
Emil Folino <emilfolino@gmail.com>
2019-09-03 18:31:52 +0200

    Fixed code examples

---
+++ main.js
@@ -0,0 +1,7 @@
+import slideorama from "slideorama";
+
+import hljs from 'highlight.js';
+import "highlight.js/styles/atom-one-dark.css";
+
+slideorama.init("presentation", "slide-container");
+hljs.initHighlightingOnLoad();

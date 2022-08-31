f3e5632d3b28873aad07091bead0c82caa1ab52e
Marie Grahn <epkmagr@gmail.com>
2022-08-31 09:38:36 +0200

    Updated code exemples lecture kmom01 again

--- example_code/ht22/kmom01/lecture/cast.py
+++ example_code/ht22/kmom01/lecture/cast.py
@@ -28,10 +28,9 @@ print(type(no_of_wheels))
 # Output
 print("En " + vehicle + " har " + no_of_wheels + " hjul.")
 
+
 # Step 3
 # Input
-# vehicle = "bil"
-# no_of_wheels = 4
 vehicle = input("Vilket fordon? ")
 no_of_wheels = int(input("Hur många hjul? "))
 
@@ -40,8 +39,6 @@ print(type(vehicle))
 print(type(no_of_wheels))
 
 # Output
-# print(vehicle)
-# print(no_of_wheels)
 # Forget to cast no_of_wheels
 print("En " + vehicle + " har " + no_of_wheels + " hjul.")
 #print("En " + vehicle + " har " + str(no_of_wheels) + " hjul.")

9596064a0e367069a0d073edaa88b54a9cf70961
Marie Grahn <epkmagr@gmail.com>
2022-09-20 14:39:17 +0200

    Add code examples to kmom03.

---
+++ example_code/ht22/kmom03/modules/greeters.py
@@ -0,0 +1,18 @@
+"""
+Several greeters
+"""
+
+def greeter():
+    """
+    Greets person
+    """
+    print("Hello ")
+    
+def goodbyer():
+    """
+    Says goodbye
+    """
+    print("Good bye ")
+
+greeter()
+goodbyer()
